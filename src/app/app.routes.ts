67f042c6f30ea136bf902bcb8f584aa682d21bc6
WillianRD <willianrodriguesdos1@gmail.com>
2025-05-17 18:34:28 -0300

    Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
 import { PageComponent } from './page/page.component';
 
-export const routes: Routes = [
-    {path: 'page', component: PageComponent}
-];
+export const routes: Routes = [];
