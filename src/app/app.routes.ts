669cdb11d6593647e3f635a3ca9a84ada0b9dd90
WillianRD <willianrodriguesdos1@gmail.com>
2025-05-24 19:30:23 -0300

    Feature...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
 import { PageComponent } from './page/page.component';
+import { LivroPageComponent } from './livro-page/livro-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path : 'app-root', component: PageComponent},
+    { path : 'app-livro-page', component: LivroPageComponent},
+    
+];
